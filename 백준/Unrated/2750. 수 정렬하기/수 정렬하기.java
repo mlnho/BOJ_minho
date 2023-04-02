426accdf1932529d72db6586db36be9c7cbe51ce
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-04-02 23:57:28 +0900

    [Unrated] Title: 수 정렬하기, Time: 340 ms, Memory: 22428 KB -BaekjoonHub

---
+++ 백준/Unrated/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		int[] array = new int[N];
+
+		for (int i = 0; i < N; i++) {
+
+			array[i] = scanner.nextInt();
+		}
+
+		Arrays.sort(array);
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(array[i]);
+			
+			
+		}
+
+	}
+}
