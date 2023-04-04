086f7bf952d1167278e666c1b4654a0bad970219
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-04-04 23:46:36 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+
+        
+        float answer = (float)num1 / num2;
+        
+
+        
+        return (int)(answer*1000);
+    }
+}
