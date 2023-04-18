746f9775105c00c0de6ea7ff352745058f7c383a
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-04-18 23:00:29 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int [2];
+        
+        
+        	for(int i=1; i<=m; i++ ) {
+			if(n%i==0 && m%i==0) {
+				answer[0] = i; //최대공약수 
+			} else {
+				continue;
+			}
+			if(n%answer[0]==0 && m%answer[0]==0) {
+				answer[1] = answer[0] * (n/answer[0]) * (m/answer[0]);
+			}
+		}
+		
+        
+        
+        return answer;
+    }
+}
