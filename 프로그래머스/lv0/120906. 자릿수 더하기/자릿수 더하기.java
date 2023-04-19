33c3d652b0f49da7a7d824b37597088f121f855e
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-04-19 23:05:40 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+       
+        String str = Integer.toString(n);
+        String [] arr = str.split("");
+        
+        for(int i =0; i<arr.length; i++){
+            answer += Integer.parseInt(arr[i]);
+        }
+            
+        
+        
+        
+        
+        return answer;
+    }
+}
