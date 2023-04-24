cb812e0226e8ebf869d90dc620c3dc2dbeafd557
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-04-25 00:09:36 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -3,14 +3,11 @@ class Solution {
     public int solution( int n) {
         int result = 0;
        
-        int i = (int)Math.sqrt(n);
-        
-        if(i * i == n){
-            result = 1;
+        if(n%(int)Math.sqrt(n)==0){
+            return 1;
         } else {
-            result = 2;
+            return 2;
         }
-        
-       return result;
+     
     }
 }
