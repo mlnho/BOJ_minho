0ebdd7c58542805dee00cadf0745ccee03f2150c
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-05-08 22:29:55 +0900

    [level 0] Title: 숫자 찾기, Time: 0.18 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        
+        String str = Integer.toString(num);
+		String [] arr = str.split("");
+		
+		for(int i=0; i<arr.length; i++) {
+			if(Integer.parseInt(arr[i]) == k) {
+				answer = i+1;
+				break;
+			} else {
+                answer = -1;
+            }
+		}
+        
+        return answer;
+    }
+}
