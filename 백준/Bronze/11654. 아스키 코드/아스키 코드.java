f3821b64ded5ba66f97881bc41e7d5a462f58be0
jin min ho <105831006+mlnho@users.noreply.github.com>
2023-05-12 23:48:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		
+		char a = scanner.nextLine().charAt(0);
+		
+		// 아스시코드를 -> 숫자로 변환
+		int b = a;
+		
+		
+		
+		System.out.println(b);
+		
+
+	}
+}
